de8d98f9f5bdf1fca05e4d3794e8e6b12acc8a11
Justinas Orechovas <jorechovas@archeti.com>
2020-08-01 14:47:32 -0400

    [#9343][13.0][IMP] base_tier_validation - Notify reviewer by sequence and notification to approbation process creator: - Added onchange for approve_sequence; - Added invisible attribute for notify_by_sequence; - Added notify_creator_full_validated field in Tier Definition view; - Sending an email to approbation process creator when record is full validated.

--- base_tier_validation/models/tier_definition.py
+++ base_tier_validation/models/tier_definition.py
@@ -64,7 +64,17 @@ class TierDefinition(models.Model):
     notify_on_create = fields.Boolean(
         string="Notify Reviewers on Creation",
         help="If set, all possible reviewers will be notified by email when "
-        "this definition is triggered.",
+             "this definition is triggered."
+    )
+    notify_by_sequence = fields.Boolean(
+        string="Notify Reviewers by Sequence",
+        help="If set, all possible reviewers will be notified by sequence when"
+             " this definition is triggered."
+    )
+    notify_creator_full_validated = fields.Boolean(
+        string="Notify Creator on Final Approbation Process",
+        help="If set, creator will be notified when"
+             " record is fully validated then this definition is triggered."
     )
     has_comment = fields.Boolean(string="Comment", default=False)
     approve_sequence = fields.Boolean(
@@ -77,6 +87,11 @@ class TierDefinition(models.Model):
     def onchange_review_type(self):
         self.reviewer_id = None
         self.reviewer_group_id = None
+
+    @api.onchange("approve_sequence")
+    def onchange_approve_sequence(self):
+        if not self.approve_sequence:
+            self.notify_by_sequence = False
 
     @api.depends("review_type", "model_id")
     def _compute_domain_reviewer_field(self):
