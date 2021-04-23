8d33081e2bdbb493e79ecfb4e12502b8159dc0a9
Justinas Orechovas <jorechovas@archeti.com>
2021-04-23 11:35:04 -0400

    [#12334][MIG] base_tier_validation - features added from ArcheTI was migrated from v13 to v14, branch (https://github.com/archeti-org/server-ux/tree/9343_13.0_base_tier_validation)

--- base_tier_validation/models/tier_definition.py
+++ base_tier_validation/models/tier_definition.py
@@ -57,6 +57,16 @@ class TierDefinition(models.Model):
         help="If set, all possible reviewers will be notified by email when "
         "this definition is triggered.",
     )
+    notify_by_sequence = fields.Boolean(
+        string="Notify Reviewers by Sequence",
+        help="If set, all possible reviewers will be notified by sequence when"
+             " this definition is triggered."
+    )
+    notify_creator_full_validated = fields.Boolean(
+        string="Notify Creator on Final Approbation Process",
+        help="If set, creator will be notified when"
+             " record is fully validated then this definition is triggered."
+    )
     has_comment = fields.Boolean(string="Comment", default=False)
     approve_sequence = fields.Boolean(
         string="Approve by sequence",
@@ -68,3 +78,8 @@ class TierDefinition(models.Model):
     def onchange_review_type(self):
         self.reviewer_id = None
         self.reviewer_group_id = None
+
+    @api.onchange("approve_sequence")
+    def onchange_approve_sequence(self):
+        if not self.approve_sequence:
+            self.notify_by_sequence = False
